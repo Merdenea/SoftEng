78a38f2a3a06324af977f750b81c8d595af1ef09
Bogdan Cristian  Anton <sonik@Bogdans-MacBook-Pro.local>
2017-11-30 18:54:57 +0000

    Fix classes

--- src/com/tfl/billing/NullArgumentException.java
+++ src/com/tfl/billing/NullArgumentException.java
@@ -1,7 +1,4 @@
 package com.tfl.billing;
 
-public class NullArgumentException extends RuntimeException {
-    public NullArgumentException(){
-        super("You cannot create a journey with null ID");
-    }
+public class NullArgumentException {
 }
